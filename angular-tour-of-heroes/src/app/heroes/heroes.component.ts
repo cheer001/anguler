40952915d71250721ca52d13cc5826977e154be0
azure2_gerry <gerrytech567@gmail.com>
2022-12-08 09:15:09 +0700

    使用Observable模拟异步

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
+import { HeroService } from './../hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './../hero';
-import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,18 +8,25 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
-  heroes = HEROES;
+  private heroService;
+  constructor(heroService: HeroService) {
+    this.heroService = heroService;
+  }
 
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  /** 获取服务中提供的getHeroes()方法返回的数据 */
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() {}
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    /** 初始化Heroes数据列表 */
+    this.getHeroes();
+  }
 }
